873e61d54eb613d7dbfc662d27a96cb3dc654fc3
Amalie Von Behren <vbamalie@gmail.com>
2022-06-04 16:34:53 -0400

    copied typeDefs from my mongoDB 3rd party modeler and copied user authentication process from activity 21-25

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/apollo-review',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/new-scenic-route',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const {AuthenticationError} = require('apollo-server-express');
+const {User} = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async()=> {
+            return User.find();
+        },
+        user: async (parent, {userId}) => {
+            return User.findOne({_id: userId})
+        },
+        me: async (parent, args, context)=>{
+            if(context.user){
+                return User.findOne({_id: context.user._id});
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+    },
+    mutation:{
+        addUser: async  (parent, {name, email, password}) => {
+            const user = await User.create({ name, email, password});
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, { username, password }) => {
+            const user = await User.findOne({ username });
+      
+            if (!user) {
+              throw new AuthenticationError('No profile with this username found!');
+            }
+      
+            const correctPw = await profile.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+          },
+    }
+};
+
+odule.exports = resolvers;
