95d123a593922bdc5538916c6ce83a1548b8ca72
GigaScott <scott.gamit.09@outlook.com>
2022-06-06 16:26:13 -0400

    added Up Voting and Down Voting for Routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 // const {AuthenticationError} = require('apollo-server-express');
-const {User, Note, Route } = require('../models');
+const { User, Note, Route } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -8,8 +8,8 @@ const resolvers = {
         //     return User.find({}).populate('routes')
         //    ;
         // },
-        user: async (parent, {userId}) => {
-            return User.findById({_id: userId})
+        user: async (parent, { userId }) => {
+            return User.findById({ _id: userId })
         },
         // me: async (parent, args, context)=>{
         //     if(context.user){
@@ -18,50 +18,68 @@ const resolvers = {
         //     }
         //     throw new AuthenticationError('You need to be logged in!')
         // },
-        routes: async () =>{
+        routes: async () => {
             return Route.find().populate('notes')
         },
-        singleRoute: async (parent, { routeId}) =>{
+        singleRoute: async (parent, { routeId }) => {
             return Route.findOne({ _id: routeId }).populate('notes')
         }
     },
-    Mutation:{
-        addUser: async  (parent, {username, email, password}) => {
-            const user = await User.create({ username, email, password});
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
             const token = signToken(user);
-            return {token, user};
+            return { token, user };
         },
         // login: async (parent, { username, password }) => {
         //     const user = await User.findOne({ username });
-      
+
         //     if (!user) {
         //       throw new AuthenticationError('No profile with this username found!');
         //     }
-      
+
         //     const correctPw = await profile.isCorrectPassword(password);
-      
+
         //     if (!correctPw) {
         //       throw new AuthenticationError('Incorrect password!');
         //     }
-      
+
         //     const token = signToken(user);
         //     return { token, user };
         // },
-        addRoute: async ( parent, args) =>{
+        addRoute: async (parent, args) => {
             const createRoute = await Route.create(args);
             const addToUser = await User.findOneAndUpdate(
-                args.routeId, {$addToSet:{routes: createRoute}, new: true}
+                args.routeId, { $addToSet: { routes: createRoute }, new: true }
             )
-            return {createRoute, addToUser}
+            return { createRoute, addToUser }
         },
-        addNote: async ( parent, args) =>{
+        addNote: async (parent, args) => {
             const createNote = await Note.create(args)
             const addToRoute = await Route.findOneAndUpdate(
-                args.routeId, {$addToSet:{notes: createNote._id}, new: true}
+                args.routeId, { $addToSet: { notes: createNote._id }, new: true }
             )
-            return {createNote, addToRoute}
-        }
-    }
-}
+            return { createNote, addToRoute }
+        },
+        //+1 the vote count on a specific route
+        upVote: async (parent, args) => {
+            const vote = await Route.findOneAndUpdate(
+                args.routeId,
+                { $inc: { vote: 1 } },
+                { new: true }
+            );
+            return vote;
+        },
+        //-1 the vote count on a specific route
+        downVote: async (parent, args) => {
+            const vote = await Route.findOneAndUpdate(
+                args.routeId,
+                { $inc: { vote: -1 } },
+                { new: true }
+            );
+            return vote;
+        },
+    },
+};
 
 module.exports = resolvers
