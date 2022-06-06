64ae70e95b81b00c26b144fd9e312b56b5975cc1
sarah attar <sarahattar@sarahs-MacBook-Pro.local>
2022-06-06 11:28:17 -0400

    addeed code for the header and footer

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>About Us </p>
+     
+    </footer>
+  );
+}
+
+
+
+    
+  export default Footer;
+    
+
+
