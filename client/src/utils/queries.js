d0bce4dbf28c102b5af1c3293dc87853e03dfe9e
Lex Matthews <tanukiguernica@gmail.com>
2022-06-07 15:10:44 -0400

    pain

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,57 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      routes {
-        geometry
-        description
-        difficultyLevel
-        title
-        votes
-        userId
-        tags
-        notes
-      }
-    }
-}
-`;
-
-export const QUERY_ROUTES = gql`
-    query getRoutes{
-        routes{
-            _id
-            geometry
-            description
-            difficultyLevel
-            title
-            votes
-            userId
-            tags
-            notes
-        }
-    }
-`;
-
-export const QUERY_SINGLE_ROUTE = gql`
-    query getSingleRoutes($routeId: ID){
-        route(roughtId: $routeId){
-            _id
-            geometry
-            description
-            difficultyLevel
-            title
-            votes
-            userId
-            tags
-            notes
-        }
-    }
-`
-
 export const QUERY_ME = gql`
   query me {
     me {
@@ -66,8 +14,60 @@ export const QUERY_ME = gql`
         votes
         userId
         tags
-        notes
       }
     }
   }
 `;
+
+// export const QUERY_USER = gql`
+//   query user($username: String!) {
+//     user(username: $username) {
+//       _id
+//       username
+//       email
+//       routes {
+//         geometry
+//         description
+//         difficultyLevel
+//         title
+//         votes
+//         userId
+//         tags
+//         notes
+//       }
+//     }
+// }
+// `;
+
+// export const QUERY_ROUTES = gql`
+//     query getRoutes{
+//         routes{
+//             _id
+//             geometry
+//             description
+//             difficultyLevel
+//             title
+//             votes
+//             userId
+//             tags
+//             notes
+//         }
+//     }
+// `;
+
+// export const QUERY_SINGLE_ROUTE = gql`
+//     query getSingleRoutes($routeId: ID){
+//         route(roughtId: $routeId){
+//             _id
+//             geometry
+//             description
+//             difficultyLevel
+//             title
+//             votes
+//             userId
+//             tags
+//             notes
+//         }
+//     }
+// `
+
