95b7dc298bbd6292fdc1cbfa113b0b7dc99832e9
Lex Matthews <tanukiguernica@gmail.com>
2022-06-07 12:17:51 -0400

    saving before merge

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import { useRef, useEffect, useState } from 'react';
 
-//This will stay most likely
+
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
@@ -11,39 +11,86 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiNGdlY2MwIiwiYSI6ImNsM3lqaXlkaTA3cXkzaGxzaHRhb
 
 
 const Map= ()=>{
-    //map state init
-const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(-70.9);
-const [lat, setLat] = useState(42.35);
-const [zoom, setZoom] = useState(9);
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
 
 
-useEffect(() => {
-  if (map.current) return; // initialize map only once
-  map.current = new mapboxgl.Map({
-  container: mapContainer.current,
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [lng, lat],
-  zoom: zoom
-  
-  })
-    const geocoder= new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: map.current,
-        fuzzyMatch: true,
-        autocomplete: false
+    useEffect(() => {
+        if (map.current) return;
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        
+        
+        })
+    
+        const geocoder= new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: map.current,
+            fuzzyMatch: true,
+            autocomplete: false,
+            proximity:{
+                "latitude": lat,
+                "longitude": lng
+            }
+        });
+      
+        map.current.addControl(new mapboxgl.GeolocateControl({trackUserLocation: false}),'top-right');
+        map.current.addControl(geocoder, 'top-left');    
+    
+        map.current.on('load', () => {
+            map.current.addSource('my-data', {
+                "type": "geojson",
+                "data": {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "LineString",
+                        "coordinates": [
+                            [-122.4833858013153, 37.829607404976734],
+                            [-122.4830961227417, 37.82932776098012],
+                            [-122.4830746650696, 37.82932776098012],
+                            [-122.48218417167662, 37.82889558180985],
+                            [-122.48218417167662, 37.82890193740421],
+                            [-122.48221099376678, 37.82868372835086],
+                            [-122.4822163581848, 37.82868372835086],
+                            [-122.48205006122589, 37.82801003030873]
+                        ]
+                    }, //CHANGE ABOVE OBJECT LATER TO BE DYNAMIC
+                }
+            })
+
+            map.current.addLayer({
+                'id': 'my-data-layer',
+                'type': 'line',
+                'source': 'my-data',
+                'paint': {
+                    'line-width': 5,
+                    // Use a get expression (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
+                    // to set the line-color to a feature property value.
+                    'line-color': 'red'
+                },
+                "minzoom": 12
+            });
         });
 
-  
-    map.current.addControl(new mapboxgl.GeolocateControl({trackUserLocation: false}),'top-right');
-    map.current.addControl(geocoder, 'top-left');
-});
+        
+
+    });
+    
+    
+
+    
+
 
   return (
-    <div>
+    <section>
         <div ref={mapContainer} className="map-container" />
-    </div>
+    </section>
   )
     
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,6 @@ const Home = () =>{
             
             <Footer />
         </main>
-
     )
 }
 
