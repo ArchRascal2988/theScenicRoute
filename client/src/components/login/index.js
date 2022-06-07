f12c545f4543823f6e2f2e626d1f01d3be3f4102
Lex Matthews <tanukiguernica@gmail.com>
2022-06-07 13:50:15 -0400

    saving before merge to test singin

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -2,21 +2,65 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from 'react-bootstrap/Button';
 
-const Login= ()=>{
+import { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+import {LOGIN_USER} from '../../utils/mutations';
+import Auth from '../../utils/auth';
+
+
+const Login= (props)=>{
+    ////LOGIN FUNCTION
+    const [formState, setFormState] = useState({ username: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+    
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+
+
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try {
+          const { data } = await login({
+            variables: { ...formState },
+          });
+    
+          Auth.login(data.login.token);
+        } catch (e) {
+          console.error(e);
+        }
+    
+        // clear form values
+        setFormState({
+          email: '',
+          password: '',
+        });
+      };
+
     return (
     <div>
         <h1>Login</h1>
-        <Form>
+        <Form onSubmit={handleFormSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Username</Form.Label>
-                <Form.Control className="usernInput" type="username" placeholder="Enter username" />
+                <Form.Control className="usernInput" type="username" placeholder="Enter username" value={formState.username}
+                  onChange={handleChange}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control className="passInput" type="password" placeholder="Password" />
+                <Form.Control className="passInput" type="password" placeholder="Password" value={formState.password}
+                  onChange={handleChange}/>
             </Form.Group>
-            <Button className='submitBtn' variant="primary" type="submit">
+            <Button variant="primary" type="submit">
                 Submit
             </Button>
         </Form>
