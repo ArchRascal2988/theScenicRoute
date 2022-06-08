f85a37addf7e221d40041870f532007db429602d
sarah attar <sarahattar@sarahs-MacBook-Pro.local>
2022-06-07 23:15:30 -0400

    added to the pages folder

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import CreateMap from "../components/createMap/index"
 
 const Create = () =>{
 
     return(
         <main>
             <Header />
+
+
+            <CreateMap  />
             
             
             <Footer />
