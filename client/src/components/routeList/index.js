f8d3b6f7ee23654a90bc237f4e720f305c109069
GigaScott <scott.gamit.09@outlook.com>
2022-06-09 07:38:36 -0400

    here we go

--- client/src/components/routeList/index.js
+++ client/src/components/routeList/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import RouteItem from '../routeInfo/index'
+import { QUERY_ME, QUERY_ROUTES, QUERY_USER } from "../utils/queries";
 
 // Accept an array of routes as a prop
 const RouteList = ({ routes }) => {
+
+  const { loading, data } = useQuery(QUERY_ROUTES);
+  const honeymoonRoutes = data?.routes || ['error']
+  console.log(honeymoonRoutes);
+
+  
   // Create a list of rendered IssueItems by using the map method on `routes`
   const renderedList = routes.map((route) => {
 
