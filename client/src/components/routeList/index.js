9f4e1f8f0ff9628bc2c5708bf9ecfe0a37d14227
sarah attar <sarahattar@sarahs-MacBook-Pro.local>
2022-06-08 22:55:16 -0400

    added styling to app.css

--- client/src/components/routeList/index.js
+++ client/src/components/routeList/index.js
@@ -4,15 +4,15 @@ import RouteItem from '../routeInfo/index'
 // Accept an array of routes as a prop
 const RouteList = ({ routes }) => {
   // Create a list of rendered IssueItems by using the map method on `routes`
-  const renderedList = routes.map((route) => {
+  // const renderedList = routes.map((route) => {
 
-    return <RouteItem key={route.id} route={route.title} />;
-  });
+  //   return <RouteItem key={route.id} route={route.title} />;
+  // });
 
   // Return our array of RouteItems wrapped inside a parent div
   return (
       <section className='userRoutes'>
-        <div className="ui relaxed divided list">{renderedList}</div>
+        {/* <div className="ui relaxed divided list">{renderedList}</div> */}
         <a href='/create' className='createBtn'>Create a new route +</a>
       </section>
   )
