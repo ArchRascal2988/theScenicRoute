1c108f55fc19c49ebe329d70c0ec8a1ae9221a88
GigaScott <scott.gamit.09@outlook.com>
2022-06-09 21:10:19 -0400

    lex does not like apples

--- client/src/pages/Dash.js
+++ client/src/pages/Dash.js
@@ -4,24 +4,26 @@ import Header from '../components/header/index';
 import Footer from '../components/footer/index';
 import Map from '../components/map/index';
 import RouteList from '../components/routeList/index';
-import { QUERY_ME, QUERY_ROUTES, QUERY_USER } from "../utils/queries";
-
+import { QUERY_ME, QUERY_ROUTES, QUERY_USER } from "../../utils/queries";
+import Auth from '../utils/auth';
+import { Navigate, useParams } from 'react-router-dom';
 
 const Dash = () => {
+    const { username: userParam } = useParams();
 
-    const { loading, data } = useQuery(QUERY_ME);
-
-    const user = data?.me || {};
-    // navigate to personal profile page if username is yours
-    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    //   return <Navigate to="/me" />;
-    // }
+    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+      variables: { username: userParam },
+    });
+ 
+    const user = data?.me || ['error'];
     console.log(user)
-
+    // navigate to personal profile page if username is yours
+    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+        return <Navigate to="/me" />;
+    }
     if (loading) {
         return <div>Loading...</div>;
     }
-
     if (!user?.username) {
         return (
             <h4>
